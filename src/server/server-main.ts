2eea1199d04ef8b8e510911ea165158d8768189f
admoores <moores.alexd@gmail.com>
2020-09-15 23:45:45 -0400

    cleanup

--- src/server/server-main.ts
+++ src/server/server-main.ts
@@ -48,35 +48,20 @@ async function configure(): Promise<void> {
 
     if ((request.response as Boom).isBoom) {
       const boomResponse = request.response as Boom;
-
       statusCode = boomResponse.output.statusCode;
     } else {
       const response = request.response as ResponseObject;
-
-      // All good responses - short circuit
       if (response.statusCode < 400) {
         return h.continue;
       }
-
       statusCode = response.statusCode;
     }
 
-    // If the requested page is not an API route and is not requesting a file
-    // or is requesting a htm(l) file, return HTML responses
     if (request.url.pathname.indexOf(`/${API_VERSION}/`) === -1
       && (request.url.pathname.indexOf('.') === -1
         || request.url.pathname.search(/\.htm(l?)$/) !== -1)) {
-      if (statusCode === 403) {
-        return h.file(Path.join(uiPath, 'static', 'forbidden.html')).code(403);
-      }
-
-      // Return the index page for any non-file request or requests for .htm(l) files
       if (statusCode === 404) {
         return h.file(Path.join(uiPath, 'index.html'));
-      }
-
-      if (statusCode >= 500) {
-        return h.file(Path.join(uiPath, 'static', 'server-error.html')).code(500);
       }
     }
 
