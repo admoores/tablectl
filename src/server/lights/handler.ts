13a5f6524193f7678be21222a55667f8e611db2f
gouda <moores.alexd@gmail.com>
2021-06-29 22:58:39 -0400

    accepts body

--- src/server/lights/handler.ts
+++ src/server/lights/handler.ts
@@ -1,12 +1,15 @@
 import { Request } from '@hapi/hapi';
+import { SetLightsObj } from './types';
 
 const ws281x = require('rpi-ws281x');
 ws281x.configure({ leds: 10, gpio: 18 });
 
 export async function setLights(req: Request): Promise<void> {
+  const values = req.payload as SetLightsObj;
+
   const pixels = new Uint32Array(10);
   for (let i = 0; i < 10; i++) {
-    pixels[i] = (255 << 16) | (255 << 8) | (255);
+    pixels[i] = (values.red << 16) | (values.blue << 8) | (values.green);
   }
 
   ws281x.render(pixels);
--- src/server/lights/schemas.ts
+++ src/server/lights/schemas.ts
@@ -2,7 +2,7 @@ import * as Joi from '@hapi/joi';
 import { join } from 'path';
 
 export const setLights = Joi.compile(Joi.object({
-  red: Joi.number().required,
-  green: Joi.number().required(),
-  blue: Joi.number().required,
+  red: Joi.number().required().min(0).max(255),
+  green: Joi.number().required().min(0).max(255),
+  blue: Joi.number().required().min(0).max(255),
 }));
