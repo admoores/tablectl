76926c50cc8d082710af69ad653320d55fb043e7
gouda <moores.alexd@gmail.com>
2021-06-30 01:36:55 -0400

    adds light-strip class

---
+++ src/server/classes/light-strip.ts
@@ -0,0 +1,24 @@
+const ws281x = require('rpi-ws281x');
+
+export class LightStrip {
+  pin: number;
+  lights: number;
+  strip: any;
+
+  constructor(lights: number, pin: number) {
+    this.pin = pin;
+    this.lights = lights
+    ws281x.configure({ leds: this.lights, pin: this.pin, stripType: 'grb', dma: 20 });
+    this.strip = ws281x;
+  }
+
+  setAll(red: number, green: number, blue: number): void {
+    const pixels = new Uint32Array(this.lights);
+
+    for (let i = 0; i < this.lights; i++) {
+      pixels[i] = (red << 16) | (green << 8) | blue;
+
+      this.strip.render(pixels);
+    }
+  }
+}
