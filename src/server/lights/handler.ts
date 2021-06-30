829effe5242182247ae84471e4e10fff13d79607
gouda <moores.alexd@gmail.com>
2021-06-30 01:35:03 -0400

    fixes rgb/grb issue

--- src/server/lights/handler.ts
+++ src/server/lights/handler.ts
@@ -1,18 +1,14 @@
 import { Request, ResponseObject, ResponseToolkit } from '@hapi/hapi';
+import { LightStrip } from '../classes/light-strip';
 import { SetLightsObj } from './types';
 
 const ws281x = require('rpi-ws281x');
-ws281x.configure({ leds: 144, gpio: 18 });
+const strip = new LightStrip(144, 18);
 
 export async function setLights(req: Request, h: ResponseToolkit): Promise<ResponseObject> {
   const values = req.payload as SetLightsObj;
 
-  const pixels = new Uint32Array(144);
-  for (let i = 0; i < 144; i++) {
-    pixels[i] = (values.red << 16) | (values.green << 8) | (values.blue);
-  }
-
-  ws281x.render(pixels);
+  strip.setAll(values.red, values.green, values.blue);
 
   return h.response().code(200);
 }
