7cee28cb56a2ef9192f0456aa9c213adaafaa6b6
gouda <moores.alexd@gmail.com>
2021-06-30 01:15:14 -0400

    testing lights

--- src/server/lights/handler.ts
+++ src/server/lights/handler.ts
@@ -2,17 +2,15 @@ import { Request, ResponseObject, ResponseToolkit } from '@hapi/hapi';
 import { SetLightsObj } from './types';
 
 const ws281x = require('rpi-ws281x');
-ws281x.configure({ leds: 10, gpio: 18 });
+ws281x.configure({ leds: 144, gpio: 18 });
 
 export async function setLights(req: Request, h: ResponseToolkit): Promise<ResponseObject> {
   const values = req.payload as SetLightsObj;
 
-  const pixels = new Uint32Array(10);
+  const pixels = new Uint32Array(144);
   for (let i = 0; i < 10; i++) {
-    pixels[i] = (values.green << 16) | (values.red << 8) | (values.blue);
+    pixels[i] = (values.red << 16) | (values.green << 8) | (values.blue);
   }
-
-  console.log(JSON.stringify(pixels))
 
   ws281x.render(pixels);
 
